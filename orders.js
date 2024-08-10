da82a28327558897cd0b5fe8bef6eaf82d11ad83
WixZ <147816024+WixZzQ@users.noreply.github.com>
2024-08-11 03:34:15 +0600

    https://github.com/WixZzQ/Responsive-Analysis

---
+++ orders.js
@@ -0,0 +1,19 @@
+const Orders =[ {
+    productName: 'JavaScript Tutorial',
+    productNumber: '85432',
+    paymentStatus: 'Due',
+    status: 'Pending'
+},
+{
+    productName: 'CSS Full Course',
+    productNumber: '78965',
+    paymentStatus: 'Refunded',
+    status: 'Declined'
+},
+{
+    productName: 'HTML & CSS Basics',
+    productNumber: '45612',
+    paymentStatus: 'Paid',
+    status: 'Active'
+},
+]
