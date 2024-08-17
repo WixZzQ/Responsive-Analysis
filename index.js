8087726a4af64ece36d619cff0d3f7167ae30611
WixZ <147816024+WixZzQ@users.noreply.github.com>
2024-08-18 00:41:56 +0600

    Added dark mode and css

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+const sideMenu = document.querySelector('aside');
+const menuBtn = document.getElementById('menu-btn');
+const closeBtn = document.getElementById('close-btn');
+
+const darkMode = document.querySelector('.dark-mode');
+menuBtn.addEventListener('click', () =>{
+    sideMenu.classList.add('show');
+    sideMenu.style.display = 'block';
+    document.body.style.overflow = 'hidden';
+});
+
+closeBtn.addEventListener('click', () =>{
+    sideMenu.classList.remove('show');
+    document.body.style.overflow = 'auto';
+    sideMenu.style.display = 'none';
+});
+
+darkMode.addEventListener('click', () =>{
+    document.body.classList.toggle('dark-mode-variables');
+    darkMode.querySelector('span:nth-child(1)').classList.toggle('active');
+    darkMode.querySelector('span:nth-child(2)').classList.toggle('active');
+});
+
+
 Orders.forEach(order =>{
     const tr = document.createElement('tr');
     const trContent = `
